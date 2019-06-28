273f77c66162f33a9ae45bbae218f760c0797933
innovegic-developer <dev.innovegic@gmail.com>
2019-06-28 15:04:30 +0530

    1st commit

---
+++ app/src/main/java/params/com/statusView/MainActivity.kt
@@ -0,0 +1,18 @@
+package params.com.statusView
+
+import android.graphics.Color
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        statusViewScroller.statusView.run {
+            currentCount = 2
+            circleFillColorCurrent = Color.RED
+        }
+    }
+}
